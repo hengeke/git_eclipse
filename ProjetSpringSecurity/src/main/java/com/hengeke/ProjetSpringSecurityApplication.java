8f14c387dfa32c76aaf8ff11b62df378a3fd4f91
Hengeke <antsa.tco@gmail.com>
2022-03-11 18:54:10 +0100

    Commit spring tool suite

---
+++ ProjetSpringSecurity/src/main/java/com/hengeke/ProjetSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.hengeke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ ProjetSpringSecurity/src/test/java/com/hengeke/ProjetSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hengeke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
